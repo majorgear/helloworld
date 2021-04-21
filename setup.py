339c0669775989b7f73cd4031333023a51e76172
John Gooch <john@goochdom.com>
2021-04-21 07:54:32 -0600

    refactored setup and packages

--- setup.py
+++ setup.py
@@ -1,21 +1,71 @@
 from setuptools import setup
-from helloworld import __version__ as version
-import pathlib
+setup()
 
-here = pathlib.Path(__file__).parent.resolve()
+# from setuptools import find_packages
+# from helloworld import __version__ as version
+# import pathlib
 
-# Get the long description from the README file
-long_description = (here / 'README.md').read_text(encoding='utf-8')
+# here = pathlib.Path(__file__).parent.resolve()
 
+# # Get the long description from the README file
+# long_description = (here / 'README.md').read_text(encoding='utf-8')
 
-setup(name='helloworld',
-      version='1.0',
-      py_modules=['helloworld'],
-      url='',
-      author='Major Gear',
-      author_email='majorgear@majorgear.tech', 
-      description='Prints Hello World'
-      long_description=long_description,
-      
-      )
+# setup(
+#       name='helloworld',
+#       version='1.0',
+#       py_modules=['helloworld'],
+#       url='https://github.com/majorgear/helloworld',
+#       author='Major Gear',
+#       author_email='majorgear@majorgear.tech', 
+#       description='Prints Hello World',
+#       long_description=long_description,
+#       long_description_content_type='text/markdown',
+#       classifiers=[  # Optional
+#             'Development Status :: 3 - Alpha',
+#             'Intended Audience :: Developers',
+#             'Topic :: Software Development :: Build Tools',
+
+#             # Pick your license as you wish
+#             'License :: OSI Approved :: MIT License',
+
+#             'Programming Language :: Python :: 3',
+#             'Programming Language :: Python :: 3.6',
+#             'Programming Language :: Python :: 3.7',
+#             'Programming Language :: Python :: 3.8',
+#             'Programming Language :: Python :: 3.9',
+#             'Programming Language :: Python :: 3 :: Only',
+#       ],
+#       extras_require={  # Optional
+#         'dev': ['check-manifest'],
+#         'test': ['coverage'],
+#       },
+      # If there are data files included in your packages that need to be
+      # installed, specify them here.
+      # package_data={  # Optional
+      #       'sample': ['package_data.dat'],
+      # },
+      # For example, the following would provide a command called `sample` which
+      # executes the function `main` from this package when invoked:
+      # entry_points={  # Optional
+      #       'console_scripts': [
+      #             'helloworld-cli = helloworld.main:main',
+      #       ],
+      # }, 
+    # List additional URLs that are relevant to your project as a dict.
+    #
+    # This field corresponds to the "Project-URL" metadata fields:
+    # https://packaging.python.org/specifications/core-metadata/#project-url-multiple-use
+    #
+    # Examples listed include a pattern for specifying where the package tracks
+    # issues, where the source is hosted, where to say thanks to the package
+    # maintainers, and where to support the project financially. The key is
+    # what's used to render the link text on PyPI.
+#     project_urls={  # Optional
+#         'Bug Reports': 'https://github.com/pypa/sampleproject/issues',
+#         'Funding': 'https://donate.pypi.org',
+#         'Say Thanks!': 'http://saythanks.io/to/example',
+#         'Source': 'https://github.com/pypa/sampleproject/',
+#     },
+
+    # )
 
